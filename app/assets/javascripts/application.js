45138c0097ad5d822e48d4f27c85c14057936af5
Henry Morgan <hexmodr@gmail.com>
2016-07-12 12:02:45 +0100

    HI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,59 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function(){
+
+  $('[data-next-panel]').on('click', nextPanel);
+
+  $('form').on('keydown', 'input,textarea', function(e){
+    if (e.keyCode != 13) return;
+    return nextPanel();
+  });
+
+  $('form').on('submit', function(e){
+    return nextPanel()
+  })
+
+  function nextPanel() {
+    $panel = $('.current_panel');
+    var valid;
+    if (valid = validate_panel($panel)) {
+      $panel.removeClass('current_panel')
+        .next('.panel').addClass('current_panel')
+        .find('input,select,textarea').first().trigger('focus')
+    }
+
+    return valid && $('.current_panel').length == 0
+  }
+
+  function validate_panel($panel) {
+    var errors_found = false;
+
+    $('[data-required]', $panel).find('input, select').each(function(){
+      $input = $(this);
+      var error = false;
+      if ($input.is('[type=checkbox]')) {
+        error = !$input.is(':checked');
+      } else {
+        error = $input.val() == '';
+      }
+      if (error) {
+        $input.closest('.field').addClass('field_with_errors')
+        errors_found = true;
+        console.error(this)
+      } else {
+        $(this).closest('.field').removeClass('field_with_errors')
+      }
+    });
+    
+    return !errors_found
+  }
+
+
+  function hasNextPanel() {
+    return !$('.panel').last().hasClass('current_panel');
+  }
+
+
+});
