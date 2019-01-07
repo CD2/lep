48aa098d8888db2a0c521505060597a7f5b462a1
Matthew Willis <matt.willis@cd2solutions.co.uk>
2019-01-07 10:03:34 +0000

    controllers file updated

--- app/controllers/employer_forms_controller.rb
+++ app/controllers/employer_forms_controller.rb
@@ -22,6 +22,26 @@ class EmployerFormsController < ApplicationController
   private
 
     def employer_form_params
-      params.require(:employer_form).permit(:name, :address_one, :address_two, :city, :county, :postcode, :sector, :number_of_employees, :contact_name, :contact_role, :contact_email, :contact_phone, :number_apprentices, :future_number_apprentices, :proportion, :future_proportion, :work_experience, :future_work_experience, :enterprise_adviser, :logo, :organisation_type)
+      params.require(:employer_form).permit(
+        :name,
+        :address_one,
+        :address_two,
+        :city,
+        :county,
+        :postcode,
+        :sector,
+        :contact_name,
+        :contact_role,
+        :contact_email,
+        :contact_phone,
+        :pledge_type_apprenticeships,
+        :pledge_type_work_inspiration,
+        :pledge_type_work_placements,
+        :pledge_type_SEND_support,
+        :pledge_type_other_information,
+        :logo,
+        :organisation_type
+      )
     end
+
 end
